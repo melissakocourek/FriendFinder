32807b473e6953dbb5908896c26b342514eb184b
Melissa Kocourek <melissakocourek@gmail.com>
2019-04-24 12:56:20 -0500

    init

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "Daenerys Targaryen ",
+        photo: "https://i.pinimg.com/564x/ac/2d/5a/ac2d5a15e357552fc107325250eaa9fd--photo-games-game-thrones.jpg ",
+        scores: [ 5, 3, 5, 4, 5, 5, 2, 2, 1, 3]
+    },
+    {
+        name: "Arya Stark ",
+        photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/b/b1/Arya_Season_8.jpg/revision/latest?cb=20190206213617 ",
+        scores: [ 2,2,5,3,4,4,4,1,4,1]
+    },
+    {
+        name: "Jon Snow",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/03/01/14/jon-snow.jpeg",
+        scores: [ 2, 1, 4, 1, 5, 3, 5, 2, 5, 1 ]
+    },
+    {
+        name: " Cersei Lannister",
+        photo: "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/syfywire_cover_media/2018/09/game-of-thrones-cersei-queen.jpg?timestamp=1536758302 ",
+        scores: [ 5,5,2,3,1,5,4,1,1,2]
+    },
+    {
+        name: "Sansa Stark ",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/SophieTurnerasSansaStark.jpg/220px-SophieTurnerasSansaStark.jpg ",
+        scores: [ 3,4,3,3,3,3,4,2,2,4]
+    },
+    {
+        name: " Tyrion Lannister ",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Tyrion_Lannister-Peter_Dinklage.jpg/220px-Tyrion_Lannister-Peter_Dinklage.jpg ",
+        scores: [ 4,5,4,3,2,4,1,2,5]
+    },
+    {
+        name: " Bran Stark ",
+        photo: "https://cdn.vox-cdn.com/thumbor/2cTo01NzzF4hzyXt__wPSoDkFnI=/0x0:1200x675/1200x800/filters:focal(504x242:696x434)/cdn.vox-cdn.com/uploads/chorus_image/image/63412343/D4MrxAdW4AABFfM.0.jpg ",
+        scores: [ 
+
+        ]
+    },
+    {
+        name: " Brienne of Tarth",
+        photo: "https://upload.wikimedia.org/wikipedia/en/9/93/Brienne_of_Tarth-Gwendoline_Christie.jpg ",
+        scores: [ 
+
+        ]
+    },
+    {
+        name: "Samwell Tarly",
+        photo: "http://www1.pictures.zimbio.com/mp/3xm098ERt94x.jpg ",
+        scores: [ 1, 5, 3, 2, 5, 1, 2, 4, 5, 1 ]
+
+
+    }];
+
+    module.exports = friends;
