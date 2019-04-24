6fa8482c377542d63d3a3e17951e607c8cdf2232
Melissa Kocourek <melissakocourek@gmail.com>
2019-04-24 13:22:17 -0500

    init

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,10 @@ var path = require("path");
 module.exports = function(app){
 
     app.use('/', function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/home.html"));
+        res.json(path.join(_dirname, "public/home.html"));
     });
 
     app.get('/survey', function (req, res) {
-        res.sendFile(path.join(_dirname, "../public/survey.html"));
+        res.json(path.join(_dirname, "public/survey.html"));
     });
 };
