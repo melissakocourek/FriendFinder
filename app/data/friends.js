52c5dc7e405c88763561050174316ea18c0b6d83
Melissa Kocourek <melissakocourek@gmail.com>
2019-04-24 21:08:34 -0500

    init

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,51 +1,51 @@
 var friends = [
 	
 	{
-		name: "Benjamin Browder",
-		photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		name: "Daenerys Targaryen",
+		photo: "https://i.pinimg.com/564x/ac/2d/5a/ac2d5a15e357552fc107325250eaa9fd--photo-games-game-thrones.jpg",
 		scores: [
 			"5",
-			"1",
-			"4",
 			"3",
-			"1",
-			"5",
-			"1",
-			"4",
-			"3",
-			"1"
-		]
-	},
-	{
-		name: "Vladamir VanDamme",
-		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"3",
-			"1",
 			"5",
 			"4",
-			"1",
 			"5",
-			"1",
-			"1",
 			"5",
+			"2",
+			"2",
+			"1",
 			"3"
 		]
 	},
 	{
-		name: "Brian Eastman",
-		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		name: "Arya Stark",
+		photo: "https://vignette.wikia.nocookie.net/gameofthrones/images/b/b1/Arya_Season_8.jpg/revision/latest?cb=20190206213617",
+		scores: [
+			"2",
+			"2",
+			"5",
+			"3",
+			"4",
+			"4",
+			"4",
+			"1",
+			"4",
+			"1"
+		]
+	},
+	{
+		name: "Jon Snow",
+		photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/03/01/14/jon-snow.jpeg",
 		score: [
-			"3",
+			"2",
 			"1",
 			"4",
 			"1",
-			"4",
+			"5",
 			"3",
 			"5",
-			"1",
+			"2",
 			"5",
-			"4"		
+			"1"		
 		]
 	}
 ];
