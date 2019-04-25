d92aebe08d3f8490e7a59420a31de15e7182af00
Melissa Kocourek <melissakocourek@gmail.com>
2019-04-24 23:22:43 -0500

    init

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,13 +3,13 @@ var Friends = [
     name: "Ross Geller",
     photo: 'https://img.buzzfeed.com/buzzfeed-static/static/2017-06/1/6/campaign_images/buzzfeed-prod-fastlane-01/how-popular-are-your-opinions-about-ross-geller-2-14231-1496313900-8_dblbig.jpg',
     scores: [
+        2.97,
         5,
+        3.45,
+        2,
         5,
-        5,
-        5,
-        5,
-        5,
-        5,
+        2.97,
+        2.97,
         5,
         5,
         5
@@ -20,16 +20,16 @@ var Friends = [
         name: "Monica Geller",
         photo: "https://data.whicdn.com/images/157621550/large.jpg",
         scores: [
-        3.9,
-        4.35,
         5,
         2,
         5,
+        5,
+        3,
         4.22,
         5,
         3.45,
-        2.97,
-        5
+        2,
+        2
         ]
         }
 
